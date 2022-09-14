4e619719323c4ffbb21e111aff3e4870f8f7ce9f
Khemmathat <khemmathat.kong@kmutt.ac.th>
2022-09-14 19:02:28 +0700

    changed on arr (was arrMid)

--- minMedMax.js
+++ minMedMax.js
@@ -1,10 +1,10 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  let arrMid = [n1, n2, n3]
-  arrMid.sort(compareFn)
+  let arr = [n1, n2, n3]
+  arr.sort(compareFn)
   let min = Math.min(n1, n2, n3)
-  let mid = arrMid[1]
+  let mid = arr[1]
   let max = Math.max(n1, n2, n3)
   let ans = {min, mid, max}
   return  ans
